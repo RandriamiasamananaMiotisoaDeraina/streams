b4ee6887dd3d5cb1da18fbe5723f6e1ed107f1c1
RANDRIAMIASAMANANA Deraina Mioty <derainamioty@gmail.com>
2026-03-24 21:44:44 +0200

    Fix create_app properly

--- app/_init_.py
+++ app/_init_.py
@@ -1,12 +1,15 @@
-# app/__init__.py
 from flask import Flask
-from app.model import db, bcrypt
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
+db = SQLAlchemy()
+bcrypt = Bcrypt()
 login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
+
     app.config['SECRET_KEY'] = 'secret'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
 
@@ -14,6 +17,7 @@ def create_app():
     bcrypt.init_app(app)
     login_manager.init_app(app)
 
+    # IMPORT DES ROUTES (IMPORTANT)
     from app.routes.auth import auth
     from app.routes.content import content
 
